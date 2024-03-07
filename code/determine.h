cab30670d1a5ce280a2e67536c05d5ed946fce01
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-07 07:04:34 -0600

    Update determine.h

---
+++ code/determine.h
@@ -0,0 +1,38 @@
+/*
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
+*/
+#ifndef DETERMINE_H
+#define DETERMINE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <fossil/xcore.h> // core framework
+#include <fossil/xutil.h> // base utilities
+#include <fossil/xstring.h> // that string lib
+
+#include "player.h"
+
+typedef enum {
+    WIN,
+    LOSE,
+    TIE
+} GameOutcome;
+
+GameOutcome determine_outcome(FishPlayer **players);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
