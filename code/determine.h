de054743d0fab5dec5e7f13db9afbb3439575f11
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-07 09:47:52 -0600

    Update determine.h

--- code/determine.h
+++ code/determine.h
@@ -34,10 +34,10 @@ typedef struct {
     int loser_index;
 } GameResult;
 
-GameOutcome determine_outcome(FishPlayer **players);
-GameResult get_game_result(FishPlayer **players);
-const_cstring get_outcome_message(GameOutcome outcome, FishPlayer **players);
-const_cstring get_result_message(GameResult result, FishPlayer **players);
+GameOutcome determine_outcome(FishPlayer *players[]);
+GameResult get_game_result(FishPlayer *players[]);
+const_cstring get_outcome_message(GameOutcome outcome, FishPlayer *players[]);
+const_cstring get_result_message(GameResult result, FishPlayer *players[]);
 const_cstring read_message_from_file(const_cstring filename);
 
 #ifdef __cplusplus
