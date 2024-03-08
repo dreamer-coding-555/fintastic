ff7b92636f02ba82be596d061a4dcddd1e4cf2af
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-08 12:20:32 -0600

    Update story.h

---
+++ code/story.h
@@ -0,0 +1,32 @@
+/*
+==============================================================================
+Author: Michael Gene Brockus (Dreamer)
+Email: michaelbrockus@gmail.com
+Organization: Fossil Logic
+Description: 
+    This file is part of the Fossil Logic project, where innovation meets
+    excellence in software development. Michael Gene Brockus, also known as
+    "Dreamer," is a dedicated contributor to this project. For any inquiries,
+    feel free to contact Michael at michaelbrockus@gmail.com.
+==============================================================================
+*/
+#ifndef STORY_H
+#define STORY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <fossil/xcore.h> // core framework
+#include <fossil/xutil.h> // base utilities
+#include <fossil/xstring.h> // that string lib
+
+#include "player.h"
+
+void play_story_mode(FishPlayer players[MAX_PLAYERS]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
