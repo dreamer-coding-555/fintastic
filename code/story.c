c4bfcd64afa2061238c4c6b32caa84f67029457e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-08 12:46:10 -0600

    Update story.c

--- code/story.c
+++ code/story.c
@@ -11,14 +11,7 @@ Description:
 ==============================================================================
 */
 #include "story.h"
-#include "display.h"
 
 void story_mode() {
-    // Display story introduction
-    fscl_console_out_color("light_cyan", "===== STORY MODE =====\n");
-
-    // Add your story text here
-    fscl_console_out("Once upon a time in the vast ocean...\n");
-    fscl_console_out("A group of fish gathered for an epic Rock-Paper-Scissors battle.\n");
-    fscl_console_out("Join our brave fish as they embark on a thrilling adventure!\n");
+    // to be implemented
 }
